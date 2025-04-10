b623360eadd0d504958b9cff1b7eaa0e56ef3bcd
ラマ　ビサル <lb1627565@gmail.com>
2025-04-10 18:37:32 +0900

    Change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
   const [text, setText] = useState("");
   const handleUpClick =(()=> setText(text.toUpperCase()))
+  const handleLoClick =(()=> setText(text.toLowerCase()))
+  const handleClearClick =(()=> setText(""))
 
   const handleOnChange = (event) => {
     setText(event.target.value);
@@ -16,8 +18,16 @@ export default function TextForm(props) {
               <textarea className="form-control border border-dark" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"  ></textarea>
         
             <button className="btn btn-primary my-2" onClick={handleUpClick}>Convert to Uppercase</button>  
+            <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>Convert to Lowercase</button>  
+            <button className="btn btn-primary my-2 mx-2" onClick={handleClearClick}>Clear Text</button>  
         </div>
-  asdadasd
+      <div className="container my-2">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        
+      </div>
+      
+
     </>
   )
 }
