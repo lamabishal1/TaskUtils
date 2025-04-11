1f29c1a9194df073951adf2098f6bac226d07143
ラマ　ビサル <lb1627565@gmail.com>
2025-04-11 19:13:18 +0900

    made about us accordian page with dark mode and light mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import About from "./components/About";
 import Navbar from "./components/navbar";
-import TextForm from "./components/TextForm";
+//import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="my-4">
-      <TextForm heading="Enter the text below to analyze."/>
+      {/*<TextForm heading="Enter the text below to analyze."/>*/}
+      <About />
       </div>
     </div>
   );
